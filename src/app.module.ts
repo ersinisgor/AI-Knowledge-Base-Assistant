04aca5158015361eeab8835bc3e568b0f1a0c7c5
Ersin Isgor <ersinisgor@hotmail.com>
2026-03-22 11:38:31 +0300

    feat(app): integrate ConfigModule for global configuration and add global validation pipes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CommonModule } from './modules/common/common.module';
 
 @Module({
-  imports: [CommonModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    CommonModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
